060a64b723f703a713ca2b915198009d1d567d02
sweeper3000 <spiketwofish@protonmail.com>
2016-09-15 20:46:20 -0400

    Add file & readme

---
+++ ThankYouCard.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class ThankYouCard {
+	private void PrintThankYou() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Thank You Card Generator");
+		System.out.println("TEMPLATE: Thank you very much for the [gift] [name]!I really liked it!");
+		System.out.println("Enter the item you received as a gift:");
+		String gift = input.nextLine();
+		System.out.println("Enter the name of the person you received the gift from:");
+		String name = input.nextLine();
+		System.out.println("Copy and paste the output below CTRL-Shift-V");
+		System.out.println("Thank you very much for the " + gift + " " + name + "! I really liked it!");
+	}
+	public static void main(String[] args) {
+		ThankYouCard printProgram = new ThankYouCard();
+		printProgram.PrintThankYou();
+	}
+}

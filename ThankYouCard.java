ecbaea8f1c363c3451d7787a059dee32390bdf2b
sweeper3000 <spiketwofish@protonmail.com>
2016-09-16 16:42:00 -0400

    Add choices

--- ThankYouCard.java
+++ ThankYouCard.java
@@ -3,13 +3,33 @@ class ThankYouCard {
 	private void PrintThankYou() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Thank You Card Generator");
-		System.out.println("CHOICES:\nThank you very much for the [gift] [name]! I really liked it!\nThanks [name] for the [gift]!\nAmazing [gift]. Thank you [name]!");
+		System.out.println("CHOICES:\nThank you very much for the [gift] [name]! I really liked it! (3)\nThanks [name] for the [gift]! (3)\nAmazing gift. Thank you [name]! (3)");
 		System.out.println("Enter the item you received as a gift:");
 		String gift = input.nextLine();
 		System.out.println("Enter the name of the person you received the gift from:");
 		String name = input.nextLine();
 		String instructions = "Copy and paste the output below CTRL-Shift-V";
-		System.out.println("Thank you very much for the " + gift + " " + name + "! I really liked it!");
+    System.out.println("Look at the choices. Enter the number you want.");
+    int choice = input.nextInt();
+
+    switch(choice)  {
+
+        case 1:
+        System.out.println("Thank you very much for the     " + gift + " " + name + "! I really liked it!");
+        break;
+
+        case 2:
+        System.out.println("Thanks " + name + " for the" + gift + "!");
+        break;
+
+        case 3:
+        System.out.println("Amazing gift! Thank you " + name + "!");
+        break;
+
+        default:
+        System.out.println("You did not enter a valid choice.");
+        break;
+    }
 	}
 	public static void main(String[] args) {
 		ThankYouCard printProgram = new ThankYouCard();

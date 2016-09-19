7f6f5c480834049e7059ff119b6eac102f9356ab
sweeper3000 <sweeper3000@users.noreply.github.com>
2016-09-19 22:09:53 +0000

    Change output to letter format (#2)
    * Put name on first line instead of the body
    
    * Add signature and input to collect it
    
    * Update readme and Java file to reflect new format.
    
    * Polish lines
    
    * Bump version to 1.2

--- ThankYouCard.java
+++ ThankYouCard.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 class ThankYouCard {
 	private void PrintThankYou() {
 		Scanner input = new Scanner(System.in);
-		System.out.println("THANK YOU CARD GENERATOR 1.1.3");
-		System.out.println("CHOICES:\nThank you very much for the [gift] [name]! I really liked it! (1)\nThanks [name] for the [gift]! (2)\nAmazing gift. Thank you [name]! (3)");
+		System.out.println("THANK YOU CARD GENERATOR 1.2");
+		System.out.println("CHOICES:\nThank you very much for the [gift]! I really liked it! (1)\nThanks for the [gift]! (2)\nAmazing gift. Thank you! (3)\nAll notes start with Dear [name],\nThe bottom is signed with [signature] which is your name.");
 		System.out.println("Enter the item you received as a gift:");
 		String gift = input.nextLine();
 		System.out.println("Enter the name of the person you received the gift from:");
 		String name = input.nextLine();
+    System.out.println("Enter your name to sign the bottom:");
+    String signature = input.nextLine();
 		String instructions = "Copy and paste the output below CTRL-Shift-V";
 		System.out.println("Look at the choices. Enter the number you want.");
 		int choice = input.nextInt();
@@ -16,17 +18,23 @@ class ThankYouCard {
 
 		case 1:
 		System.out.println(instructions);
-		System.out.println("Thank you very much for the " + gift + " " + name + "! I really liked it!");
+		System.out.println("Dear " + name + ",");
+		System.out.println("Thank you very much for the " + gift + "! I really liked it!");
+		System.out.println("Love, " + signature);
 		break;
 
 		case 2:
 		System.out.println(instructions);
-		System.out.println("Thanks " + name + " for the " + gift + "!");
+		System.out.println("Dear " + name + ",");
+		System.out.println("Thanks for the " + gift + "!");
+		System.out.println("Love, " + signature);
 		break;
 
 		case 3:
 		System.out.println(instructions);
-		System.out.println("Amazing gift! Thank you " + name + "!");
+		System.out.println("Dear " + name + ",");
+		System.out.println("Amazing gift! Thank you!");
+		System.out.println("Love, " + signature);
 		break;
 
 		default:
